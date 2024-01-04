a0aae39fcbea670d2e9f92ffbb2f3a350aece352
mohammad ghobadi <mohammadrezaghobadi.1383@gmail.com>
2024-01-04 21:38:57 +0330

    build database order and models order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,19 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Product extends Model
+class Order extends Model
 {
     use HasFactory, SoftDeletes;
 
-    public function productable()
+    public function product()
     {
-        return $this->morphTo();
-    }
-
-    public function order()
-    {
-        return $this->belongsTo(Order::class);
+        return $this->hasMany(Product::class);
     }
 }
