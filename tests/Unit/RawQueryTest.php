01814908e596f5e7fb1edaf4bba311e26cdf2809
Sajjad Mohammadi <mohammadisajjad54@gmail.com>
2024-01-08 11:07:01 +0330

    finish test one

--- tests/Unit/RawQueryTest.php
+++ tests/Unit/RawQueryTest.php
@@ -3,7 +3,10 @@
 namespace Tests\Unit;
 
 use App\Models\Order;
+use App\Models\OrderItem;
 use App\Models\User;
+use Database\Factories\OrderItemFactory;
+use DB;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -19,5 +22,17 @@ class RawQueryTest extends TestCase
         $user = User::factory()->create();
         Order::factory()->withUser($user)->count(15)->create();
         dd($user->orders()->count());
+    }
+    public function test_one()
+    {
+        # Retrieve user details along with the uncompleted orders they have placed.
+        $user = User::factory()->create();
+        $orders = Order::factory()->withUser($user)->count(15)->create();
+        $orders->each(function (Order $order) {
+            OrderItem::factory()->create(['order_id' => $order->id]);
+        });
+        $users = DB::select('
+SELECT * FROM users LEFT JOIN orders ON (users.id = orders.user_id) LEFT JOIN order_items ON (orders.id = order_items.order_id) WHERE (orders.status = "uncompleted")');
+        dd($users);
     }
 }
