22600bfefa18ea714b7a8cf2e7423f77af948629
Sajjad Mohammadi <mohammadisajjad54@gmail.com>
2024-01-08 11:59:33 +0330

    fix test one and finish test two

--- tests/Unit/RawQueryTest.php
+++ tests/Unit/RawQueryTest.php
@@ -2,17 +2,20 @@
 
 namespace Tests\Unit;
 
+use App\Models\Category;
 use App\Models\Order;
 use App\Models\OrderItem;
+use App\Models\Product;
+use App\Models\ProductCategory;
 use App\Models\User;
-use Database\Factories\OrderItemFactory;
-use DB;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 
 class RawQueryTest extends TestCase
 {
     use RefreshDatabase;
+
     /**
      * A basic unit test example.
      */
@@ -23,6 +26,7 @@ class RawQueryTest extends TestCase
         Order::factory()->withUser($user)->count(15)->create();
         dd($user->orders()->count());
     }
+
     public function test_one()
     {
         # Retrieve user details along with the uncompleted orders they have placed.
@@ -31,8 +35,26 @@ class RawQueryTest extends TestCase
         $orders->each(function (Order $order) {
             OrderItem::factory()->create(['order_id' => $order->id]);
         });
-        $users = DB::select('
-SELECT * FROM users LEFT JOIN orders ON (users.id = orders.user_id) LEFT JOIN order_items ON (orders.id = order_items.order_id) WHERE (orders.status = "uncompleted")');
-        dd($users);
+        $usersWithOrder = DB::select('
+SELECT * FROM users INNER JOIN orders ON (users.id = orders.user_id) WHERE (orders.status = "uncompleted")');
+        dd($usersWithOrder);
+    }
+
+    public function test_two()
+    {
+        # Display all products and their associated categories. Include products without categories.
+        $products = Product::factory(20)->create();
+        Category::factory(20)->create();
+        $products->each(function (Product $product) {
+            ProductCategory::factory()->create([
+                'category_id' => $product->id,
+                'product_id' => $product->id,
+            ]);
+        });
+        Product::factory(20)->create();
+
+        $productsWithCategory = DB::select('SELECT * FROM products LEFT OUTER JOIN (product_category,categories)
+    ON (products.id = product_category.product_id AND product_category.category_id = categories.id)');
+        dd($productsWithCategory);
     }
 }
