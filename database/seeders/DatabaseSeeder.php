14c684592277bf59a8207f17e871adcf1d7c6fbd
mohammad ghobadi <mohammadrezaghobadi.1383@gmail.com>
2024-01-11 18:54:13 +0330

    query project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,13 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Category;
-use App\Models\Order;
-use App\Models\OrderItem;
-use App\Models\Product;
-use App\Models\ProductCategory;
-use App\Models\Review;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,16 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $products = Product::factory(10)->create();
-        $products->each(function (Product $product){
-            Review::factory()->create([
-                'reviewable_id' => $product->id,
-                'reviewable_type' => 'Product'
-            ]);
-        });
-        Review::factory(10)->create([
-            'reviewable_id' => 1,
-            'reviewable_type' => 'Product'
-        ]);
+//        $products = Product::factory(10)->create();
+//        $products->each(function (Product $product){
+//            Review::factory()->create([
+//                'reviewable_id' => $product->id,
+//                'reviewable_type' => 'Product'
+//            ]);
+//        });
+//        Review::factory(10)->create([
+//            'reviewable_id' => 1,
+//            'reviewable_type' => 'Product'
+//        ]);
     }
 }
